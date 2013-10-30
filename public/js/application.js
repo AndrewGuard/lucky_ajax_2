2b87775f5723c76e0b28ecc25f86b6faf2a9145a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-30 10:02:35 -0500

    functioning ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  $('.roll_button').click(function(event) {
+    event.preventDefault();
+    var roll = Math.floor((Math.random()*6)+ 1);
+    console.log(roll)
+    $.post('/rolls', {value: roll}, function(response) {
+      $('#die').html(response);
+    });
+  });
+});
+
 
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
@@ -6,5 +16,3 @@ $(document).ready(function () {
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
-
-});
